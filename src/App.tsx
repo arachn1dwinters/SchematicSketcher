d6f61ed73312f77e5e556a132710e1b3555a2aca
Arachn1d <isaacwinters3000@gmail.com>
2026-02-16 17:55:09 -0800

    Test Animations

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
-import { Stage, Text, Container } from "@pixi/react";
-import { TextStyle } from "pixi.js";
+import { Stage, Text } from "@pixi/react";
+import { TextStyle, Ticker } from "pixi.js";
+import { useRef, useEffect, useState } from "react";
 import Viewport from "./Viewport";
 import useWindowDimensions from "./UseWindowDimensions";
 
@@ -16,8 +17,8 @@ function App() {
   return (
     <Stage width={width} height={height}>
       <Viewport width={width} height={height}>
-        <Text
-          text=":)"
+        <ClickMe
+          text="Click Me :)"
           style={style}
           anchor={0.5}
           x={width / 2}
@@ -27,5 +28,49 @@ function App() {
     </Stage>
   );
 }
+
+type ClickMeProps = {
+  text: string;
+  style: TextStyle;
+  anchor: number;
+  x: number;
+  y: number;
+};
+
+const ClickMe = ({ text, style, anchor, x, y }: ClickMeProps) => {
+  const [alpha, setAlpha] = useState(1);
+  const fading = useRef(false);
+
+  useEffect(() => {
+    const ticker = new Ticker();
+
+    ticker.add((delta) => {
+      if (!fading.current) return;
+
+      setAlpha((a) => Math.max(0, a - 0.01 * delta));
+    });
+
+    ticker.start();
+
+    return () => ticker.stop();
+  }, []);
+
+  const clicked = () => {
+    fading.current = true;
+  };
+
+  return (
+    <Text
+      text={text}
+      style={style}
+      anchor={anchor}
+      x={x}
+      y={y}
+      alpha={alpha}
+      interactive
+      pointerdown={clicked}
+    />
+  );
+};
 
 export default App;
