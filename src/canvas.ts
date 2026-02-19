14dc3fd28be3579020a8b35ed23a838d58e542d0
arachn1dwinters <isaacwinters3000@gmail.com>
2026-02-19 08:59:10 -0800

    Drawing :)

--- src/canvas.ts
+++ src/canvas.ts
@@ -1,9 +1,22 @@
 import * as PIXI from "pixi.js";
 import { $ } from "jquery";
 
-let body = document.body, html = document.documentElement;
-const height = $(window).height();
-const width = $(window).width();
+// Get current mouse position
+let mousePosition : PIXI.Point = new PIXI.Point(-1, -1);
+$(document).mousemove((e) => {
+    mousePosition.x = e.pageX;
+    mousePosition.y = e.pageY;
+})
+
+// Constants etc.
+enum ACTIONS {
+    Wire,
+    Part,
+    Select,
+}
+
+let CurrentAction: ACTIONS = ACTIONS.Wire;
+let currentlyDrawing = false;
 
 const app = new PIXI.Application();
 
@@ -15,28 +28,44 @@ await app.init({
 
 document.body.appendChild(app.canvas);
 
-// Animate
-let elapsed = 0.0;
-app.ticker.add((delta) => {
-    elapsed += delta;
-});
-
 // Draw grid of dots
-const graphics = new PIXI.Graphics();
-graphics.cullable = true;
+const bgGraphics = new PIXI.Graphics();
+bgGraphics.cullable = true;
 
 for (let i = 0; i < 100; i++) {
     for (let e = 0; e < 100; e++) {
         const cornerIncrements = 5;
         const corner = (i % cornerIncrements == 0 && e % cornerIncrements == 0);
         if (!corner) {
-            graphics.circle(20*e + 10, 20*i + 10, 2.5);
-            graphics.fill({ r: 0, g: 0, b: 0, a: 0.1 });
+            bgGraphics.circle(20*e + 10, 20*i + 10, 2.5);
+            bgGraphics.fill({ r: 0, g: 0, b: 0, a: 0.1 });
         } else {
-            graphics.circle(20 * e + 10, 20 * i + 10, 3);
-            graphics.fill({ r: 0, g: 0, b: 0, a: 0.2 });
+            bgGraphics.circle(20 * e + 10, 20 * i + 10, 3);
+            bgGraphics.fill({ r: 0, g: 0, b: 0, a: 0.2 });
         }
     }
 }
 
-app.stage.addChild(graphics);
+app.stage.addChild(bgGraphics);
+
+// Wire
+let wireStart : PIXI.Point = null;
+const wireGraphics = new PIXI.Graphics();
+
+$(window).on('click', () => {
+    if (CurrentAction === ACTIONS.Wire) {
+        currentlyDrawing = !currentlyDrawing;
+        if (currentlyDrawing && wireStart == null) {
+            wireStart = new PIXI.Point(0, 0);
+        }
+    }
+})
+
+app.ticker.add((time) => {
+    if (currentlyDrawing && CurrentAction === ACTIONS.Wire) {
+        wireGraphics.roundRect(mousePosition.x, mousePosition.y, 15, 15, 20);
+        wireGraphics.fill(0x35cc5a);
+    }
+});
+
+app.stage.addChild(wireGraphics);
