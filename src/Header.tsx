d9f0f57523644d8ba0d98e19a62ce17396c58252
arachn1dwinters <isaacwinters3000@gmail.com>
2026-03-10 13:43:07 -0700

    set up gsap; set up svgr; animated component bank symbol

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,24 +1,45 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { useRef, useEffect, useState } from "react";
+import { gsap } from "gsap";
+import Switch from "./assets/ComponentBankSymbol.svg?react";
 
 function App() {
   const headerStyle = {
     position: "sticky",
     top: "0",
     backgroundColor: "#ffffea",
+    display: "flex",
+    justifyContent: "right",
+    cursor: "default",
   };
 
   const imageBankSymbolStyle = {
-    right: "0",
+    width: "75px",
+    height: "auto",
   };
+
+  const container = useRef(null);
+
+  useEffect(() => {
+    if (container.current) {
+      const arm = container.current.querySelector("#SwitchArm");
+
+      container.current.addEventListener("mouseenter", () => {
+        gsap.to(arm, { rotation: -25, transformOrigin: "0% 50%", duration: 0.4 });
+      });
+
+      container.current.addEventListener("mouseleave", () => {
+        gsap.to(arm, { rotation: 0, duration: 0.4 });
+      });
+    }
+  });
 
   return (
     <header style={headerStyle}>
-      <img style={imageBankSymbolStyle}
-        id="imageBankSymbol" 
-        src="./src/assets/ComponentBankSymbol.svg">
-      </img>
+      <div ref={container}>
+        <Switch style={imageBankSymbolStyle}/>
+      </div>
     </header>
   );
 }
